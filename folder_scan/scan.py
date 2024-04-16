523afa12c82ac3f79af5e40b77b292b6c4e5b2fa
Christopher <christopher.lin@thoughtswinsystems.com>
2024-04-16 11:57:29 -0700

    2nd commit

--- folder_scan/scan.py
+++ folder_scan/scan.py
@@ -1,11 +1,13 @@
 import os  
 import time  
+from datetime import datetime  
 import tkinter as tk  
 from tkinter import messagebox  
   
 def count_files_in_directory(directory):  
     try:  
-        return len([name for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))])  
+        with os.scandir(directory) as it:  
+            return sum(1 for entry in it if entry.is_file())  
     except FileNotFoundError:  
         print(f"The directory {directory} was not found.")  
         return -1  
@@ -15,12 +17,17 @@ def show_notification():
     root.withdraw()  # We don't want a full GUI, so keep the root window from appearing  
     messagebox.showinfo("Notification", "The number of files has not changed.")  
     root.destroy()  
+    # print("The number of files has not changed.")
   
 def monitor_directory(directory, interval_minutes):  
     prev_count = count_files_in_directory(directory)  
+    sleep_time = interval_minutes * 60  # Convert minutes to seconds
+    if prev_count != -1:  
+        print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] Initial file count: {prev_count} ")  
+        print(f"Sleep for every {sleep_time} secs")
       
     while True:  
-        time.sleep(interval_minutes * 60)  # Convert minutes to seconds  
+        time.sleep(sleep_time)  # Convert minutes to seconds  
         current_count = count_files_in_directory(directory)  
           
         if current_count == -1:  
@@ -28,8 +35,9 @@ def monitor_directory(directory, interval_minutes):
   
         if current_count == prev_count:  
             show_notification()  
+            break  # Exit the loop if the file count hasn't changed
         else:  
-            print(f"File count changed: {prev_count} -> {current_count}")  
+            print(f"[{datetime.now().strftime('%Y-%m-%d %H:%M:%S')}] File count changed: {prev_count} -> {current_count} ")  
   
         prev_count = current_count  # Update the previous count  
   
